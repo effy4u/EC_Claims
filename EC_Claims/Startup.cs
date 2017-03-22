13d16917f633171c3e883c22a50d103dd6238ed5
PAPI\Ekpenyong <king@house603.com>
2017-03-22 14:21:46 +0700

    Add project files.

---
+++ EC_Claims/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EC_Claims.Startup))]
+namespace EC_Claims
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
